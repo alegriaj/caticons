40bff0d662b4024ee774cde5dab5334ef4cbc041
Javier Alegria <javier.c.alegria@gmail.com>
2021-11-23 10:43:45 -0800

    Implement cat portraits

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,13 +3,12 @@ import React from 'react';
 const Card = ({ name }) => {
   return (
     <div className='bg-light-green br3 pa3 ma2 grow bw2 shadow-5'>
-      {/* <img
+      <img
         alt={name}
-        height='150'
-        width='150'
-        src=''
-        //src={`https://robohash.org/${name}?200x200`}
-      /> */}
+        height='200'
+        width='200'
+        src={`https://robohash.org/${name}.png?set=set4&size=200x200`}
+      />
       <div>
         <h2>{name}</h2>
       </div>
